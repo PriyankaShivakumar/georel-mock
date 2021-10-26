6d97deb4fb32405c14a269d66e39836335f5ced2
PriyankaShivakumar <priyanka.r.s@sap.com>
2021-10-26 14:58:16 +0530

    Improved performance

--- georel/tests/srv/external/API_BUSINESS_PARTNER.js
+++ georel/tests/srv/external/API_BUSINESS_PARTNER.js
@@ -22,14 +22,22 @@ module.exports = async srv => {
             const businessPartner = req.params[0].BusinessPartner;
             let entity = 'A_BusinessPartnerRole';
             let columnsToSelect = ["BusinessPartner", "BusinessPartnerRole", "ValidFrom", "ValidTo", "AuthorizationGroup"];
-            const tx = srv.transaction();
-            const resultUsingHeaders = await tx.send({
-                query: SELECT.from(entity)
-                    .columns(columnsToSelect).where({
-                        BusinessPartner: businessPartner
-                    })
-            })
+            const tx = srv.transaction(req);
+            const resultUsingHeaders = await tx.run(SELECT.from(entity).columns(columnsToSelect).where({
+                BusinessPartner: businessPartner
+            }));
             return resultUsingHeaders;
+
+
+
+            // const tx = srv.transaction();
+            // const resultUsingHeaders = await tx.send({
+            //     query: SELECT.from(entity)
+            //         .columns(columnsToSelect).where({
+            //             BusinessPartner: businessPartner
+            //         })
+            // })
+            // return resultUsingHeaders;
 
         } catch (err) {
             req.reject(err);
@@ -43,14 +51,20 @@ module.exports = async srv => {
             const businessPartner = req.params[0].BusinessPartner;
             let entity = 'A_BusinessPartnerAddress';
             let columnsToSelect = ["BusinessPartner", "AddressID", "Country", "CityName"];
-            const tx = srv.transaction();
-            const resultUsingHeaders = await tx.send({
-                query: SELECT.from(entity)
-                    .columns(columnsToSelect).where({
-                        BusinessPartner: businessPartner
-                    })
-            })
+            const tx = srv.transaction(req);
+            const resultUsingHeaders = await tx.run(SELECT.from(entity).columns(columnsToSelect).where({
+                BusinessPartner: businessPartner
+            }));
             return resultUsingHeaders;
+
+            // const tx = srv.transaction();
+            // const resultUsingHeaders = await tx.send({
+            //     query: SELECT.from(entity)
+            //         .columns(columnsToSelect).where({
+            //             BusinessPartner: businessPartner
+            //         })
+            // })
+            // return resultUsingHeaders;
 
         } catch (err) {
             req.reject(err);
@@ -65,15 +79,21 @@ module.exports = async srv => {
             const addressID = req.params[0].AddressID;
             let entity = 'A_AddressEmailAddress';
             let columnsToSelect = ["AddressID", "Person", "OrdinalNumber", "EmailAddress"];
-            const tx = srv.transaction();
-            const resultUsingHeaders = await tx.send({
-                query: SELECT.from(entity)
-                    .columns(columnsToSelect).where({
-                        //  BusinessPartner: businessPartner,
-                        AddressID: addressID
-                    })
-            })
+            const tx = srv.transaction(req);
+            const resultUsingHeaders = await tx.run(SELECT.from(entity).columns(columnsToSelect).where({
+                AddressID: addressID
+            }));
             return resultUsingHeaders;
+
+            // const tx = srv.transaction();
+            // const resultUsingHeaders = await tx.send({
+            //     query: SELECT.from(entity)
+            //         .columns(columnsToSelect).where({
+            //             //  BusinessPartner: businessPartner,
+            //             AddressID: addressID
+            //         })
+            // })
+            // return resultUsingHeaders;
 
         } catch (err) {
             req.reject(err);
@@ -87,16 +107,23 @@ module.exports = async srv => {
             const addressID = req.params[0].AddressID;
             let entity = 'A_AddressPhoneNumber';
             let columnsToSelect = ["AddressID", "Person", "OrdinalNumber", "PhoneNumber"];
-            const tx = srv.transaction();
-            const resultUsingHeaders = await tx.send({
-                query: SELECT.from(entity)
-                    .columns(columnsToSelect).where({
-                        //  BusinessPartner: businessPartner,
-                        AddressID: addressID
-
-                    })
-            })
+            const tx = srv.transaction(req);
+            const resultUsingHeaders = await tx.run(SELECT.from(entity).columns(columnsToSelect).where({
+                AddressID: addressID
+            }));
             return resultUsingHeaders;
+
+
+            // const tx = srv.transaction();
+            // const resultUsingHeaders = await tx.send({
+            //     query: SELECT.from(entity)
+            //         .columns(columnsToSelect).where({
+            //             //  BusinessPartner: businessPartner,
+            //             AddressID: addressID
+
+            //         })
+            // })
+            // return resultUsingHeaders;
 
         } catch (err) {
             req.reject(err);
